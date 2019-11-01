fc1749d69824f99410510ea58cb46327ec17ad7a
GMohn <57241178+GMohn@users.noreply.github.com>
2019-10-31 18:24:12 -0700

    Add files via upload

---
+++ proj3/testsixmensmorris.cpp
@@ -0,0 +1,43 @@
+#include <gtest/gtest.h> 	  			 	 
+#include "SixMensMorrisBoard.h"
+
+TEST(SixMensMorrisBoardTest, DefaultBoardTest){
+    // Needs to test that default board is correct
+}
+
+TEST(SixMensMorrisBoardTest, SetBoardTest){
+    // Needs to test that setting board is correct
+}
+
+TEST(SixMensMorrisBoardTest, ResetBoardTest){
+    // Needs to test that resetting to default board is correct
+}
+
+TEST(SixMensMorrisBoardTest, PlacementTest){
+    // Needs to test that normal placement is correct
+}
+
+TEST(SixMensMorrisBoardTest, PlacementMillTest){
+    // Needs to test that placement creating a mill is correct with removal
+}
+
+TEST(SixMensMorrisBoardTest, MoveTest){
+    // Needs to test that movement is correct
+}
+
+TEST(SixMensMorrisBoardTest, MoveMillTest){
+    // Needs to test that movement creating a mill is correct with removal
+}
+                                       
+TEST(SixMensMorrisBoardTest, TwoPieceGameOverTest){
+    // Needs to test that game over is correct when only two pieces left
+}
+
+TEST(SixMensMorrisBoardTest, NoMoveGameOverTest){
+    // Needs to test that game over is correct when no movement allowed
+}
+
+TEST(SixMensMorrisBoardTest, BadParametersTest){
+    // Needs to test that correct return upon bad parameters
+}
+
