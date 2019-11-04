4d0437cdf0c2bd8ec29e2b35b122b346d03a9a85
GMohn <gmohn@ucdavis.edu>
2019-11-04 02:38:48 -0800

    Added some tests. subject to change
    //TODO Find how to call a private method

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ SixMensMorrisBoard:
 https://www.fluentcpp.com/2018/06/05/10-techniques-that-will-make-you-understand-other-peoples-code-better/
 https://www.learncpp.com/cpp-tutorial/bit-manipulation-with-bitwise-operators-and-bit-masks/
 https://youtu.be/vf0n7te3_Vc
+
+testsixmensmorris:
+https://github.com/google/googletest/blob/master/googletest/docs/advanced.md#testing-private-code
--- proj3/src/testsixmensmorris.cpp
+++ proj3/src/testsixmensmorris.cpp
@@ -32,6 +32,8 @@ TEST(SixMensMorrisBoardTest, DefaultBoardTest){
 
 TEST(SixMensMorrisBoardTest, SetBoardTest){
     // Needs to test that setting board is correct
+	CSixMensMorrisBoard Board;
+
     char turn = SIX_MENS_MORRIS_PLAYER_W;
     int unplaced[SIX_MENS_MORRIS_PLAYERS] = {2,3};
     char positions[SIX_MENS_MORRIS_POSITIONS] = {};
@@ -40,33 +42,83 @@ TEST(SixMensMorrisBoardTest, SetBoardTest){
 
 TEST(SixMensMorrisBoardTest, ResetBoardTest){
     // Needs to test that resetting to default board is correct
+	CSixMensMorrisBoard Board;
+	EXPECT_TRUE(Board.Place(SIX_MENS_MORRIS_PLAYER_R, 15));
+
+	EXPECT_TRUE(Board.Place(SIX_MENS_MORRIS_PLAYER_W, 9));
+	EXPECT_EQ(Board.PlayerAtPosition(9), SIX_MENS_MORRIS_PLAYER_W);
+
+	EXPECT_TRUE(Board.Place(SIX_MENS_MORRIS_PLAYER_R, 8));
+	EXPECT_EQ(Board.PlayerAtPosition(8), SIX_MENS_MORRIS_PLAYER_R);
+
+	EXPECT_FALSE(Board.Place(SIX_MENS_MORRIS_PLAYER_W, 15));
+	EXPECT_EQ(Board.PlayerAtPosition(15), SIX_MENS_MORRIS_PLAYER_R);
+	
+	Board.ResetBoard();
+	EXPECT_EQ(Board.UnplacedPieces(SIX_MENS_MORRIS_PLAYER_R), SIX_MENS_MORRIS_PIECES);
+	EXPECT_EQ(Board.UnplacedPieces(SIX_MENS_MORRIS_PLAYER_W), SIX_MENS_MORRIS_PIECES);
+	for (int index = 0; index < SIX_MENS_MORRIS_POSITIONS; index++) {
+		EXPECT_EQ(Board.PlayerAtPosition(index), SIX_MENS_MORRIS_EMPTY);
+	}
+	
+
 }
 
 TEST(SixMensMorrisBoardTest, PlacementTest){
     // Needs to test that normal placement is correct
+	CSixMensMorrisBoard Board;
+	EXPECT_TRUE(Board.Place(SIX_MENS_MORRIS_PLAYER_R, 0));
+
+	EXPECT_TRUE(Board.Place(SIX_MENS_MORRIS_PLAYER_W, 6));
+
+
+	EXPECT_TRUE(Board.Place(SIX_MENS_MORRIS_PLAYER_R, 3));
+
+
+	EXPECT_FALSE(Board.Place(SIX_MENS_MORRIS_PLAYER_W, 0));
+	EXPECT_EQ(Board.PlayerAtPosition(0), SIX_MENS_MORRIS_PLAYER_R);
 }
 
 TEST(SixMensMorrisBoardTest, PlacementMillTest){
     // Needs to test that placement creating a mill is correct with removal
+	//place at 2,9,15 expect mill created
+	CSixMensMorrisBoard Board;
+	Board.Place(SIX_MENS_MORRIS_PLAYER_R, 2);
+	Board.Place(SIX_MENS_MORRIS_PLAYER_W, 3);
+	Board.Place(SIX_MENS_MORRIS_PLAYER_R, 9);
+	Board.Place(SIX_MENS_MORRIS_PLAYER_W, 10);
+	Board.Place(SIX_MENS_MORRIS_PLAYER_R, 15);
+	//TODO Find how to call a private method
+	//EXPECT_TRUE(CSixMensMorrisBoard::MillCreated(SIX_MENS_MORRIS_PLAYER_R));
+	Board.Place(SIX_MENS_MORRIS_PLAYER_W, 7);
+	//TODO ...
+	//EXPECT_TRUE(Board.MillCreated(SIX_MENS_MORRIS_PLAYER_W));
+	
 }
 
 TEST(SixMensMorrisBoardTest, MoveTest){
     // Needs to test that movement is correct
+	CSixMensMorrisBoard Board;
+	//test piece on 6 -> 0 and that 0 ! -> 3
 }
 
 TEST(SixMensMorrisBoardTest, MoveMillTest){
     // Needs to test that movement creating a mill is correct with removal
+	CSixMensMorrisBoard Board;
 }
                                        
 TEST(SixMensMorrisBoardTest, TwoPieceGameOverTest){
     // Needs to test that game over is correct when only two pieces left
+	CSixMensMorrisBoard Board;
 }
 
 TEST(SixMensMorrisBoardTest, NoMoveGameOverTest){
     // Needs to test that game over is correct when no movement allowed
+	CSixMensMorrisBoard Board;
 }
 
 TEST(SixMensMorrisBoardTest, BadParametersTest){
     // Needs to test that correct return upon bad parameters
+	CSixMensMorrisBoard Board;
 }
 
